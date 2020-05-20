b6e1d8c3153ff532a7efbb321cd4b71acc97ec63
Luis Oliveira <luis_oliveira@usp.br>
2020-05-20 15:43:03 -0300

    Há esperanças

--- tline.py
+++ tline.py
@@ -3,66 +3,45 @@ l = 10000       #comprimento da linha de transmissao (m)
 uf = 0.9*c      #velocidade do sinal de tensao (m/s)
 t_estac = 10*l/uf
 DeltaZ = l/100
-DeltaT = DeltaZ/uf
+DeltaT = DeltaZ/uf * 0.5
 Kmax = 100
 Nmax = int(t_estac/DeltaT)
 Z0 = 50.0     # impedancia caracteristica
-Zg = 75.0     # impedancia interna da fonte
+Vg = 2       # tensao da fonte
+Rs = 75.0     # impedancia interna da fonte
+Rl = 100
 
 C = 1/(Z0*uf)
 L = Z0*Z0*C
 
-c1 = -DeltaT/(DeltaZ*L)
-c2 = 1.0
-c3 = -DeltaT/(DeltaZ*C)
-c4 = 1.0
-
-V = {}     # tensao na linha de transmissao
+V = {}      # tensao na linha de transmissao
+V_ = {}     # tensao calculada para simplificação de contas
 I = {}      # corrente na linha de transmissao
-#Vinst = []  # tensao instantanea
-#Iinst = []  # corrente instantanea
-Vg = 1       # tensao da fonte
 
-# V(0,0) atribuido
-V[0,0] = (Z0*Vg/(Zg+Z0))
-# V(k,0) atribuidos
-for k in range(1,Kmax):
-    V[k,0] = 0.0
+#constantes
+c1 = 2*DeltaT/(Rs*C*DeltaZ)
+c2 = 2*DeltaT/(Rl*C*DeltaZ)
+c3 = DeltaT*DeltaT/(L*C*DeltaZ*DeltaZ)
 
-# I(0,0) atribuido
-I[0,0] = (Vg/(Zg+Z0))
-# I(k,0) atribuidos
-for k in range(1,Kmax):
-    I[k,0] = 0.0
-
-'''for n in range(1,Nmax):
-    for k in range(0,Kmax):
-        if k == 0:
-            I[k,n] = c1*(V[k,n-1]) + c2*I[k,n-1]
-        elif k != (Kmax-1):
-            I[k,n] = c1*(V[k,n-1]-V[k-1,n-1]) + c2*I[k,n-1]
-        else:
-            I[k,n] = c1*(-V[k-1,n-1]) + c2*I[k,n-1]
-    for k in range(0,Kmax):
-        if k != Kmax-1:
-            V[k,n] = c3 * (I[k+1,n] - I[k,n]) + c4 * V[k,n-1]
-        else:
-            V[k,n] = c4 * V[k,n-1]'''
+#condicoes iniciais no tempo 0
+for k in range(0,Kmax):
+    V_[k,0] = 0
+    if(k!=Kmax-1):
+        I[k,0] = 0
 
 for n in range(1,Nmax):
-    for k in range(0,Kmax):
-        if k == 0:
-            I[k,n] = c1 * (V[k,n-1]) + c2 * I[k,n-1]
-        else:
-            I[k,n] = c1 * (V[k,n-1] - V[k-1,n-1]) + c2 * I[k,n-1]
-    for k in range(0,Kmax):
-        if k == Kmax-1:
-            V[k,n] = c4 * V[k,n-1]
-        else:
-            V[k,n] = c3 * (I[k+1,n] - I[k,n]) + c4 * V[k,n-1]
-
-for n in range(50):
     for k in range(Kmax):
-        print(V[k,n], " ", end='')
+        if k == 0:
+            V_[k,n] = (1-c1) * V_[k,n-1] - 2*I[k,n-1] + 2/Rs*Vg
+        elif k == Kmax-1:
+            V_[k,n] = (1-c2) * V_[k,n-1] + 2*I[k-1,n-1]
+        else:
+            V_[k,n] = V_[k,n-1] - (I[k,n-1] - I[k-1,n-1])
+    for k in range(Kmax-1):
+        I[k,n] = I[k,n-1] - c3*(V_[k+1,n] - V_[k,n])
+
+for n in range(Nmax):
+    for k in range(Kmax-1):
+        print(I[k,n], " ", end='')
     print()
     
