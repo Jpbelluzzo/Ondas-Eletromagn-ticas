844dd986deab7501620e93b95f1a84b7ce264aa0
Jpbelluzzo <jpbelluzzo@usp.com>
2020-07-05 02:36:08 -0300

    2.7 funcionando

--- Projeto II/ex27.py
+++ Projeto II/ex27.py
@@ -1,34 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 
-S = 1
-u = {}
+# Definicao dos numeros de Courant e do vetor para plotagem
+S = [1, 0.5]
+aux = []
 
-for i in range(200):
-    if i<40:
-        u[0,i] = 1
-    else:
-        u[0,i] = 0
-
-for n in range(199):
-    for i in range(200):
-        if (i!=0 and i!=199):
-            u[n+1,i] = (S**2) * (u[n,i+1]-2*u[n,i]+u[n,i-1]) + 2*u[n,i] 
-        elif i==0:
-            u[n+1,i] = (S**2) * (u[n,i+1]-2*u[n,i]) + 2*u[n,i] 
+# Execucao do algoritmo para todos os valores de S
+for s in S:
+    u = {}
+    u_plot = []
+    N = int(200/s)
+    for n in range(N):          # Sinal da fonte
+        if n < 40/s:
+            u[n, 0] = 1
         else:
-            u[n+1,i] = (S**2) * (-2*u[n,i]+u[n,i-1]) + 2*u[n,i]
-        if n!=0: u[n+1,i]-=u[n-1,i]
+            u[n, 0] = 0
 
-u_plot = []
+    for i in range(1, 200):
+        u[0, i] = 0
 
-for i in range(200):
-    u_plot.append(u[1,i])
+    for n in range(1, N):       # Execucao do algoritmo FDTD em si
+        j = 1 if n < 40/s else 0
+        for i in range(j, 200):
+            if (i != 0 and i != 199):
+                u[n, i] = (s**2) * (u[n-1, i+1] - 2*u[n-1, i] +
+                                    u[n-1, i-1]) + 2*u[n-1, i]
+            elif i == 0:
+                u[n, i] = (s**2) * (u[n-1, i+1] - 2*u[n-1, i]) + 2*u[n-1, i]
+            else:
+                u[n, i] = (s**2) * (- 2*u[n-1, i] + u[n-1, i-1]) + 2*u[n-1, i]
+            if n != 1:
+                u[n, i] -= u[n-2, i]
+    for i in range(200):
+        u_plot.append(u[int(160/s), i])
+    aux.append(u_plot)
 
+# Plotagem do gráfico
 t1 = np.arange(0, 200, 1)
 
 plt.figure()
-plt.plot(t1, u_plot, 'k')
+plt.plot(t1, aux[0], 'r--', label="S = 1")
+plt.plot(t1, aux[1], 'k', label="S = 0.5")
+plt.xlabel("Coordenada i da malha")
+plt.ylabel("Funcao de onda u(i)")
+plt.legend(loc="upper left")
 
 plt.show()
